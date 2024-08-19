f63df15d7d91f82ab06c4d3245553d97732ae9b4
Ahmet H. <128539459+hacioglua@users.noreply.github.com>
2024-08-19 03:52:22 +0300

    Update setup-linux.sh

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-if ! command -v docker &> /dev/null
-then
-    echo "Installing Docker"
-    sudo apt-get update
-    sudo apt-get install -y docker.io
-else
-    echo "Docker installed."
-fi
+sudo apt-get update
+sudo apt-get install -y docker docker-compose
 
-
-if ! command -v docker-compose &> /dev/null
-then
-    echo "Installing Docker Compose..."
-    sudo apt-get install -y docker-compose
-else
-    echo "Docker Compose installed."
-fi
-
-
-if ! systemctl is-active --quiet docker
-then
-    echo " Starting Docker..."
-    sudo systemctl start docker
-else
-    echo "Docker is already"
-fi
+sudo systemctl start docker
 
 docker-compose up --build -d
